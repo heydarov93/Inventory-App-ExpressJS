173a2eb6034a1ad984a1d593f0977b88e7b90d73
heydarov93 <heyderov.yashar@mail.ru>
2025-03-27 13:24:48 +0400

    Create error middleware.

--- app.js
+++ app.js
@@ -14,6 +14,21 @@ app.use("/", indexRouter);
 app.use("/categories", categoryRouter);
 app.use("/items", itemRouter);
 
+app.use((error, req, res, next) => {
+  console.log(error);
+
+  if (!error.statusCode) {
+    res
+      .status(500)
+      .render("error", { statusCode: 500, message: "Internal Server Error" });
+    return;
+  }
+
+  res
+    .status(error.statusCode)
+    .render("error", { statusCode: error.statusCode, message: error.message });
+});
+
 const PORT = process.env.APP_PORT;
 app.listen(PORT, () => {
   console.log("Application runs on port ", PORT);
