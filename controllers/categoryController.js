f83d21254de865bf19da29ea4ac7d37ae37ebb0d
heydarov93 <heyderov.yashar@mail.ru>
2025-04-02 18:02:10 +0400

    Implement form validation for category form using express-validator library.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,31 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
 const CustomNotFoundError = require("../error/CustomNotFoundError");
+const { body, validationResult } = require("express-validator");
+
+const postFormValidation = [
+  body("category_name")
+    .trim()
+    .notEmpty()
+    .withMessage("Category name is required.")
+    .isLength({ min: 3 })
+    .withMessage("Category name must have minimum 3 characters."),
+  body("confirm_secret_key")
+    .trim()
+    .notEmpty()
+    .withMessage("Confirmation of secret key is required.")
+    .isLength({ min: 5 })
+    .withMessage("Secret key must have minimum 5 characters."),
+];
+
+const putFormValidation = [
+  ...postFormValidation,
+  body("secret_key")
+    .optional({ values: "falsy" })
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("Secret key must have minimum 5 characters."),
+];
 
 // Get all items of a category and render them
 const getItemsByCategory = asyncHandler(async (req, res) => {
@@ -46,28 +71,73 @@ const displayForm = asyncHandler(async (req, res) => {
 });
 
 // Insert a new category
-const insertCategory = asyncHandler(async (req, res) => {
-  const { category_name, confirm_secret_key } = req.body;
+const insertCategory = [
+  postFormValidation,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
 
-  await db.insertCategory({ category_name, secret_key: confirm_secret_key });
+    // If there is any error
+    if (!errors.isEmpty()) {
+      const options = {
+        isItemForm: false,
+        category: null,
+        title: "Add new category",
+        heading_1: "Add new category",
+        errors: errors.errors,
+      };
 
-  res.redirect("/");
-});
+      // Render same form with errors and related options
+      res.status(400).render("form", options);
+      return;
+    }
+
+    const { category_name, confirm_secret_key: secret_key } = req.body;
+
+    await db.insertCategory({ category_name, secret_key });
+
+    res.redirect("/");
+  }),
+];
 
 // Update a category
-const updateCategory = asyncHandler(async (req, res) => {
-  const { category_id, category_name, secret_key, confirm_secret_key } =
-    req.body;
+const updateCategory = [
+  putFormValidation,
+  asyncHandler(async (req, res) => {
+    const { category_id, category_name, secret_key, confirm_secret_key } =
+      req.body;
 
-  await db.updateCategory({
-    category_id,
-    category_name,
-    secret_key: secret_key || confirm_secret_key, // if user doesn't change secret key so it should stay same as previous one
-    confirm_secret_key,
-  });
+    const errors = validationResult(req);
 
-  res.redirect(`/categories/${category_id}/edit`);
-});
+    // If there is any error
+    if (!errors.isEmpty()) {
+      const options = {
+        isItemForm: false,
+        category: {
+          category_id,
+          category_name,
+          secret_key,
+          confirm_secret_key,
+        },
+        title: "Update category #" + category_id,
+        heading_1: "Update category #" + category_id,
+        errors: errors.errors,
+      };
+
+      // Render same form with errors and related options
+      res.status(400).render("form", options);
+      return;
+    }
+
+    await db.updateCategory({
+      category_id,
+      category_name,
+      secret_key: secret_key || confirm_secret_key, // if user doesn't change secret key so it should stay same as previous one
+      confirm_secret_key,
+    });
+
+    res.redirect(`/categories/${category_id}/edit`);
+  }),
+];
 
 module.exports = {
   getItemsByCategory,
