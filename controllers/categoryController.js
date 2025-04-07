fd2ec4887fe18623afcae18531068d6812a6c6fa
heydarov93 <heyderov.yashar@mail.ru>
2025-04-07 22:08:15 +0400

    Validate category id via express-validator.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
 const CustomNotFoundError = require("../error/CustomNotFoundError");
-const { body, validationResult } = require("express-validator");
+const { body, validationResult, param } = require("express-validator");
 
 const postFormValidation = [
   body("category_name")
@@ -27,48 +27,77 @@ const putFormValidation = [
     .withMessage("Secret key must have minimum 5 characters."),
 ];
 
+const paramsValidation = [
+  param("categoryId")
+    .custom(async (id, { req }) => {
+      const category = await db.getCategoryById(id);
+
+      if (!category || category.length === 0) throw new Error();
+
+      if (
+        req.originalUrl.match(/^\/categories\/[^/]+\/edit$/) &&
+        req.method === "GET"
+      )
+        req.category = category;
+
+      return true;
+    })
+    .withMessage("Category not found."),
+];
+
 // Get all items of a category and render them
-const getItemsByCategory = asyncHandler(async (req, res) => {
-  const { categoryId } = req.params;
+const getItemsByCategory = [
+  paramsValidation,
+  asyncHandler(async (req, res) => {
+    const { categoryId } = req.params;
+    const errors = validationResult(req);
 
-  const items = await db.getItemsByCategory(categoryId);
+    // Throw error if category not found for a given id
+    if (!errors.isEmpty()) throw new CustomNotFoundError(errors.errors[0].msg);
 
-  // If items is falsy value or if it is an empty array throw not found error
-  if (!items || !items.length)
-    throw new CustomNotFoundError("No items found for this category.");
+    // If category id is valid and category exists then query db for relative items
+    const items = await db.getItemsByCategory(categoryId);
 
-  res.render("items", {
-    title: items[0].category_name,
-    heading_1: `All items for ${items[0].category_name} category`,
-    items,
-  });
-});
+    // If items is falsy value or if it is an empty array throw not found error
+    if (!items || !items.length)
+      throw new CustomNotFoundError("No items found for this category.");
 
-// Display form to inserr/update a category
-const displayForm = asyncHandler(async (req, res) => {
-  const { categoryId } = req.params;
-  const options = {
-    isItemForm: false,
-    category: null,
-    title: "Add new category",
-    heading_1: "Add new category",
-  };
+    res.render("items", {
+      title: items[0].category_name,
+      heading_1: `All items for ${items[0].category_name} category`,
+      items,
+    });
+  }),
+];
 
-  // If category id exists display update form with category data
-  if (categoryId) {
-    const category = await db.getCategoryById(categoryId);
+// Display form to insert/update a category
+const displayForm = [
+  paramsValidation,
+  asyncHandler(async (req, res) => {
+    const { categoryId } = req.params;
+    const options = {
+      isItemForm: false,
+      category: null,
+      title: "Add new category",
+      heading_1: "Add new category",
+    };
 
-    // If category is falsy value or if it is an empty array throw not found error
-    if (!category || !category.length)
-      throw new CustomNotFoundError("Category not found.");
+    // If category id exists display update form with category data
+    if (categoryId) {
+      const errors = validationResult(req);
 
-    options.title = "Update category #" + categoryId;
-    options.heading_1 = "Update category #" + categoryId;
-    options.category = category[0];
-  }
+      // Throw error if category not found for a given id
+      if (!errors.isEmpty())
+        throw new CustomNotFoundError(errors.errors[0].msg);
 
-  res.render("form", options);
-});
+      options.title = "Update category #" + categoryId;
+      options.heading_1 = "Update category #" + categoryId;
+      options.category = req.category[0];
+    }
+
+    res.render("form", options);
+  }),
+];
 
 // Insert a new category
 const insertCategory = [
